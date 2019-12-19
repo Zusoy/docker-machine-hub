a61e8086f8baddafa0323dcca69cadee3d3b6f46
Zusoy <gregoire.drapeau79@gmail.com>
2019-12-19 21:20:59 +0100

    Added machine types

--- src/app/machine-inspector/machine-inspector.component.ts
+++ src/app/machine-inspector/machine-inspector.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DockerMachineService } from '../services/docker-machine.service';
+import { Machine } from 'src/types';
 
 @Component({
   selector: 'app-machine-inspector',
@@ -8,12 +9,17 @@ import { DockerMachineService } from '../services/docker-machine.service';
 })
 export class MachineInspectorComponent implements OnInit
 {
-    private displayedColumns: string[] = ['name', 'ip', 'driver'];
-    private machines: object[] = [];
+    private columns: string[] = ['name', 'state', 'ip', 'driver'];
+    private machines: Machine[] = [];
 
     constructor (private docker: DockerMachineService) {}
 
     public ngOnInit(): void
+    {
+        this.syncMachines();
+    }
+
+    public syncMachines(): void
     {
         this.machines = this.docker.inspectAllMachines();
         console.log(this.machines);
